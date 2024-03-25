a8d6ea01f0ac502f9d6c2508278f51886219ce56
Daniel Vivas <danieldinosaurio77@gmail.com>
2024-03-25 02:18:04 -0600

    Projecto ATM

---
+++ pantalla.js
@@ -0,0 +1,23 @@
+let userdata = {}
+const form = document.querySelector("#form_deposito")
+const botonSalir = document.querySelector("#salir")
+const movimientos = document.querySelector('#movimientos')
+
+document.addEventListener("DOMContentLoaded", function () {    
+    const correctUserString = sessionStorage.getItem("correctUser");    
+    const correctUser = JSON.parse(correctUserString);
+    userdata = correctUser;        
+});
+
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+    const dataform = new FormData(form)
+    const { deposito } = Object.fromEntries(dataform)
+    userdata.saldo = Number(userdata.saldo) + Number(deposito)    
+    sessionStorage.setItem("correctUser", JSON.stringify(userdata))
+    movimientos.innerHTML = `Se realizo correctamnete su deposito por: $ ${deposito}`
+})
+
+botonSalir.addEventListener("click", function(){
+    window.location.href = "./atm.html"
+})
